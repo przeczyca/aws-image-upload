cd93c538c2d71ea32aaabb47e3b4f6223e712ee1
przeczyca <przeczyca@gmail.com>
2021-10-26 17:13:53 -0500

    can upload images to aws

--- src/main/java/pr/awsimageupload/profile/UserProfileController.java
+++ src/main/java/pr/awsimageupload/profile/UserProfileController.java
@@ -5,13 +5,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -37,7 +31,8 @@ public class UserProfileController {
                 produces = MediaType.APPLICATION_JSON_VALUE
                 )
 
-    public void uploadUserProfileImage(@PathVariable("userProfileId")UUID userProfileId, @RequestParam("file") MultipartFile file){
-        UserProfileService.uploadUserProfileImage(userProfileId, file);
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file") MultipartFile file){
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
--- src/main/java/pr/awsimageupload/profile/UserProfileService.java
+++ src/main/java/pr/awsimageupload/profile/UserProfileService.java
@@ -1,8 +1,12 @@
 package pr.awsimageupload.profile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
+import pr.awsimageupload.bucket.BucketName;
+import pr.awsimageupload.filestore.FileStore;
+
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,10 +15,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserProfileService {
     
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService){
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore){
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     public List<UserProfile> getUserProfiles(){
@@ -22,11 +28,45 @@ public class UserProfileService {
     }
 
     //remove "static" at some point
-    public static void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
         // 1. Check if image is not empty
+        isFileEmpty(file);
+
         // 2. If file is an image
+        isImage(file);
+        
         // 3. The user exists in database
+        UserProfile user = getUserProfiles()
+            .stream()
+            .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+
         // 4. Grab some metadata from file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
         // 5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+
+        try{
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e){
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file){
+        if (file.isEmpty()) throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + " ]");
+    }
+
+    private void isImage(MultipartFile file){
+        if (!Arrays.asList(ContentType.IMAGE_JPEG.getMimeType(),
+                           ContentType.IMAGE_PNG.getMimeType(),
+                           ContentType.IMAGE_GIF.getMimeType()).contains(file.getContentType())){
+            throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
+        }
     }
 }
