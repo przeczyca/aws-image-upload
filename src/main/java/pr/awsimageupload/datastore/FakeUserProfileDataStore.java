12c8625729b386c280aec71f160dfe7618c75ea4
przeczyca <przeczyca@gmail.com>
2021-10-27 14:17:26 -0500

    minor changes
    added application.properties to .gitignore so aws keys won't be public
    
    set uuid of users to specific string
    
    uses original file name for aws image

--- src/main/java/pr/awsimageupload/datastore/FakeUserProfileDataStore.java
+++ src/main/java/pr/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -14,8 +14,8 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static{
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "janetjones", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "antoniojunior", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("88eb5a52-c278-460e-a062-2bdca86cf9e6"), "janetjones", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("fc7182b4-d845-4eda-840d-5047e8500015"), "antoniojunior", null));
     }
 
     public List<UserProfile>  getUserProfiles(){
